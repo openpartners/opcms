f1b55fed5f87963fc5586345dac931824f2ea0ad
openpartners <andrzejsew@gmail.com>
2013-01-17 10:59:34 +0100

    test opcms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opcms::Application.config.secret_token = '661f162d915014cdfa67be79984208ba10710afa03d638f7318b1f33c1de506b39e7917802737594365aaab505aa3f4ca922ef05f64d2aeb1cf2d16adf2daf62'
